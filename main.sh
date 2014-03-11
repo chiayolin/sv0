32c4d4b1193bcd067a94520e6d5ac6ebeba8cf25
chiayolin <chiayo.lin@gmail.com>
2014-03-11 22:23:04 +0700

    Added files

---
+++ main.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# installtion
+LIST_OF_APPS="msmtp-mta heirloom-mailx curl openssh-server"
+apt-get install -y $LIST_OF_APPS
+
+# Set up configuration, and place files.
+cat ./data/msmtprc.txt > ~/.msmtprc 
+cat ./data/mailrc.txt > ~/.mailrc
+cat ./data/sendip > /etc/network/if-up.d/sendip 
+chmod 755 /etc/network/if-up.d/sendip 
+# Add a Linux user, test
+if [ $(id -u) -eq 0 ]; then
+username="test"
+password="password"
+egrep "^$username" /etc/passwd >/dev/null
+pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+useradd -m -p $pass $username
+sudo adduser test sudo
+fi
+
+# Exit
+exit 0
